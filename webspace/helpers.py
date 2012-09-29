12bb5490bd3756e787e0ba86a7ae06ed071c4703
Shane A <shaneallgeier@gmail.com>
2012-09-28 23:52:03 -0500

    Made sure that int_to_ascii works for 0

--- webspace/helpers.py
+++ webspace/helpers.py
@@ -5,7 +5,7 @@ def int_to_ascii(number):
     while number > 0:
         number, remainder = divmod(number, radix)
         str = valid_chars[remainder] + str
-    return str
+    return str or valid_chars[0]
         
 int_to_ascii.valid_chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-+=._~'
 int_to_ascii.radix = len(int_to_ascii.valid_chars)
