56b93cf5c357d0afe98deb15a99b418fcdf1c0bc
Shane A <shaneallgeier@gmail.com>
2012-09-28 22:38:28 -0500

    Setup the app templates

--- webspace/urls.py
+++ webspace/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('webspace.views',
-    url(r'', 'index', name='index'),
+    url(r'^$', 'index', name='index'),
 )
--- webspace/views.py
+++ webspace/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
-def index( request ):
-    return HttpResponse( "Hello World" )
+
+def index(request):
+    return render_to_response('webspace/index.html')
