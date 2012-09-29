a64bc3b599707b6fc3894230c540a37add364747
Shane A <shaneallgeier@gmail.com>
2012-09-28 23:06:08 -0500

    Added a quick file-upload form

--- webspace/views.py
+++ webspace/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 
 
 def index(request):
-    return render_to_response('webspace/index.html')
+    return render(request, 'webspace/index.html')
